c09ce41fa2bec19a4e0c66bfba19cf90964ad932
Brandon Lim Qi Hui <brandonlqh@gmail.com>
2019-10-10 16:21:56 +0800

    #2 Setup app.js

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+/* --- Setup: View engine --- */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,19 +20,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+/* --- Error handling for 404s --- */
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+/* --- Regular error handling --- */
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
+  res.locals.error = req.app.get('env') === 'development' ? err : {}; // Only provide error in dev
+  res.status(err.status || 500); // Rendering error page
   res.render('error');
 });
 
