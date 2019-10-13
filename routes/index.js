65fd219c734660a65118c95885811856098e6892
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-10-13 18:44:41 +0800

    Subscribe to MQTT Broker

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Guardians of the Wanderers' });
+router.get("/", function(req, res, next) {
+  res.render("pages/index");
+});
+
+var mqtt = require("mqtt");
+var client = mqtt.connect("mqtt://broker.mqtt.dashboard.com:1883");
+
+client.on("connect", function() {
+  // subscribe to a topic
+  client.subscribe("gotw/warn", function(err) {
+    // when a message arrives, do something with it
+    client.on("message", function(topic, message, packet) {
+      console.log("Received '" + message + "' on '" + topic + "'");
+    });
+  });
 });
 
 module.exports = router;
